8c3a7295da04269a53793323df51eb80d13250b2
zhouguangming <zhouguangming1989@gmail.com>
2012-06-16 22:40:27 +0800

    add attr_accessable logged_exception.rb

--- app/models/exception_logger/logged_exception.rb
+++ app/models/exception_logger/logged_exception.rb
@@ -1,5 +1,6 @@
 module ExceptionLogger
   class LoggedException < ActiveRecord::Base
+	attr_accessible :exception_class, :controller_name, :action_name, :message, :backtrace, :request
     class << self
       def create_from_exception(controller, exception, data)
         message = exception.message.inspect
